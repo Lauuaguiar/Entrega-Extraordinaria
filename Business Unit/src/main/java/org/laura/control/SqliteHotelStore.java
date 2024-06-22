8ca38abec3c184f6278a90fa9c986cc9f93315d2
Lauraaguiar <145450311+Lauuaguiar@users.noreply.github.com>
2024-06-22 13:14:23 +0000

    Corrections after the tutorial: truncate the temperature and other variables of type double to two decimal places.

--- Business Unit/src/main/java/org/laura/control/SqliteHotelStore.java
+++ Business Unit/src/main/java/org/laura/control/SqliteHotelStore.java
@@ -43,7 +43,7 @@ public class SqliteHotelStore implements HotelStore {
             createTable(connection, hotel.getLocation().getIsland());
             String tableName = hotel.getLocation().getIsland();
             String selectQuery = "SELECT * FROM \"" + tableName + "\" WHERE id=?";
-            String insertQuery = "INSERT INTO \"" + tableName + "\" (\"ID\", \"Name\", \"Stars\", \"TS\") VALUES (?, ?, ?, ?)";
+            String insertQuery = "INSERT INTO \"" + tableName + "\" (\"TS\", \"ID\", \"Name\", \"Stars\") VALUES (?, ?, ?, ?)";
             try (PreparedStatement selectStatement = connection.prepareStatement(selectQuery)) {
                 selectStatement.setString(1, hotel.getId());
                 try (ResultSet resultSet = selectStatement.executeQuery()) {
